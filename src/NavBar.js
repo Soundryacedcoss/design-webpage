e2751d017088953ab321058a1a3f2561bcd812ab
Soundryacedcoss <soundryarani@cedcoss.com>
2022-11-28 16:27:09 +0530

    Done

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,19 +3,19 @@ import img1 from './image/Screenshot_2022-11-01_11-12-59.png'
 export const NavBar = () => {
   return (
     <div>
-        
             <nav className='navbar' >
             <div className='logobuttonDiv'>
             <button className='logobutton'><img src={img1} alt="" /></button>
             </div>
             <div className='links'>
+              {/* Links */}
                <ul className='flex ul'>
                 <li><a href="">Solution</a></li>
                 <li><a href="">Products</a></li>
                 <li><a href="">Resources</a></li>
                 <li><a href="">Company</a></li>
                 <li><a href="">Get Started</a></li>
-                <li><input type="text" placeholder='&#x1F50E;Search' /></li>
+                <li><a href="">'&#x1F50E;Search</a></li>
                 </ul>     
             </div>
             </nav>
--- src/Page.js
+++ src/Page.js
@@ -3,12 +3,13 @@ import img1 from './image/Screenshot_2022-11-01_11-12-59 (1).png'
 export const Page = () => {
   return (
     <div className='page flex'>
+        
         <div className='flex'>
             <div className='containt'>
                     <h2>Get Started with apptio Today</h2>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo fugit delectus tenetur, provident facilis asperiores?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus iusto reiciendis adipisci at quos corporis?</p>
             </div>
-            <div className='Signup'>
+            {/* <div className='Signup'>
                <div className='flex'>
                     <div>
                        <input type="text" placeholder='first name' />
@@ -28,16 +29,26 @@ export const Page = () => {
                 <div><input type="text" placeholder='Job Function' /></div>
             </div>
             <br />
-            <div>
+            <div className='numberDiv'>
                 <input type="number" placeholder='contry' />
             </div>
             <br />
-            <div> <textarea name="" id="" cols="45" rows="6" placeholder='How can we help?'></textarea> </div>
+            <div> <textarea className='textarea' name="" id="" cols="4" rows="8" placeholder='How can we help?'></textarea> </div>
             <br />
             <div className='startedButtonDiv' >
                  <button className='startedButton'>Get Started</button>
                 </div>
-            </div>
+            </div> */}
+            <table style={{marginLeft:"70px"}}>
+                <tr><td> <input type="text" placeholder='first name' /></td> <td><input type="text" placeholder='last' /></td> </tr>
+                <tr><td><input type="text" placeholder='work Email' /></td><td><input type="text" placeholder='Phone' /></td></tr>
+                <tr><td><input type="text" placeholder='Company' /></td> <td><input type="text" placeholder='Job Function' /></td> </tr>
+                <tr><td><input type="number" placeholder='contry' /></td></tr>
+                <tr><td> <textarea className='textarea' name="" id="" cols="4" rows="8" placeholder='How can we help?'></textarea></td></tr>
+                <div className='startedButtonDiv' >
+                 <button className='startedButton'>Get Started</button>
+                </div>
+            </table>
             </div>
             <div className='logo'>
                 <button className='logobutton'><img src={img1} alt="" srcset="" /></button>
